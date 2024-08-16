05146b8dacb11912ab46f26d3cc9c2527b59c081
RoyerYampasi <royeryl03@gmail.com>
2024-08-16 20:00:23 -0300

    fix tabla provedor

--- src/models/Provedor.js
+++ src/models/Provedor.js
@@ -8,7 +8,7 @@ module.exports = (sequelize) => {
          autoIncrement: true
       },
       razonSocial:{
-         type:DataTypes.INTEGER,
+         type:DataTypes.STRING,
          allowNull:false,
       },
       cuit:{
